3382e7d85716b86b7d9f7dc47f2ad3d3bce37bcc
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-08-31 17:16:35 +0500

    Create routes file and add basic routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/routes.jsx
@@ -0,0 +1,18 @@
+import App from "./App.jsx";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/sign-up",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <App />,
+  },
+];
+
+export default routes;
