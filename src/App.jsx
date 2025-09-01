e6cd299ee71b6e866f74b8e05ae7d3277aadd3a9
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-01 14:17:37 +0500

    Add navigations to the basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import SignUp from "./Pages/SignUp/SignUp";
 import BlogCardsSection from "./Components/BlogCardsSection/BlogCardsSection";
+import Login from "./Pages/Login/Login";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  const locationPathName = location.pathname;
+
   return (
     <>
       <Header />
-      <BlogCardsSection />
+      {locationPathName === "/sign-up" ? (
+        <SignUp />
+      ) : locationPathName === "/login" ? (
+        <Login />
+      ) : (
+        <BlogCardsSection />
+      )}
     </>
   );
 }
