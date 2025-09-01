5b7a74d4bf85fe2f00c7ed38b248d41601299b16
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-01 14:18:12 +0500

    Create login page and implement signing up functionality

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,30 @@
-import styles from './Login.module.css';
+import AuthContent from "../../Components/AuthContent/AuthContent";
+import Field from "../../Components/Field/Field";
+import FormButton from "../../Components/FormButton/FormButton";
+
+import styles from "./Login.module.css";
+
 
 function Login() {
-
+  return (
+    <div className={styles.loginWrapper}>
+      <div className={styles.login}>
+        <AuthContent />
+        <form className={styles.loginForm} action="">
+          <Field name="Email" />
+          <Field name="Username" />
+          <Field name="Password" />
+          <FormButton buttonText="Login" />
+          <div className={styles.loginLinkToSignUp}>
+            Don't have an account?
+            <a href="" className={styles.loginLinkToSignUp}>
+              Create account
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import AuthContent from "../../Components/AuthContent/AuthContent";
 import Field from "../../Components/Field/Field";
 import FormButton from "../../Components/FormButton/FormButton";
 import styles from "./SignUp.module.css";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function signUpUser() {
+    await fetch("http://localhost:3000/api/auth/sign-up", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, username, password }),
+    });
+    
+    useNavigate("/login");
+  }
+
   return (
     <div className={styles.signUpWrapper}>
       <div className={styles.signUp}>
         <AuthContent />
-        <form className={styles.signUpForm} action="">
-          <Field name="Email" />
-          <Field name="Username" />
-          <Field name="Password" />
+        <form className={styles.signUpForm} action={signUpUser}>
+          <Field name="Email" handleChange={setEmail} />
+          <Field name="Username" handleChange={setUsername} />
+          <Field name="Password" handleChange={setPassword} />
           <FormButton buttonText="Sign Up" />
           <div className={styles.signUpLinkToLogin}>
             Already Have an Account?
