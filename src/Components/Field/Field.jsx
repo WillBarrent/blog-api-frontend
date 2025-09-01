3cac5418e64c064b80adb6424d61a5b5f1500396
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-01 14:19:43 +0500

    Handle input state changes with useState

--- src/Components/Field/Field.jsx
+++ src/Components/Field/Field.jsx
@@ -1,10 +1,17 @@
 import styles from "./Field.module.css";
 
-function Field({ name }) {
+function Field({ name, handleChange }) {
   return (
     <div className={styles.field}>
-      <label className={styles.fieldLabel} htmlFor={`field-${name}`}>{name}</label>
-      <input className={styles.fieldInput} type="text" id={`field-${name}`} />
+      <label className={styles.fieldLabel} htmlFor={`field-${name}`}>
+        {name}
+      </label>
+      <input
+        className={styles.fieldInput}
+        type={name === "Password" ? "password" : "text"}
+        id={`field-${name}`}
+        onChange={(e) => handleChange(e.target.value)}
+      />
     </div>
   );
 }
