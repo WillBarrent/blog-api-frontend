cf778ae335a2da7a45b5585578621b933321289e
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-02 19:49:12 +0500

    Add login functionality with token being saved in local storage

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
 import AuthContent from "../../Components/AuthContent/AuthContent";
 import Field from "../../Components/Field/Field";
 import FormButton from "../../Components/FormButton/FormButton";
 
 import styles from "./Login.module.css";
 
-
 function Login() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login() {
+    const user = await fetch("http://localhost:3000/api/auth/login", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, username, password }),
+    });
+
+    const token = (await user.json()).token;
+
+    localStorage.setItem("token", `Bearer ${token}`);
+  }
+
   return (
     <div className={styles.loginWrapper}>
       <div className={styles.login}>
         <AuthContent />
-        <form className={styles.loginForm} action="">
-          <Field name="Email" />
-          <Field name="Username" />
-          <Field name="Password" />
+        <form className={styles.loginForm} action={login}>
+          <Field name="Email" handleChange={setEmail}/>
+          <Field name="Username" handleChange={setUsername}/>
+          <Field name="Password" handleChange={setPassword}/>
           <FormButton buttonText="Login" />
           <div className={styles.loginLinkToSignUp}>
             Don't have an account?
