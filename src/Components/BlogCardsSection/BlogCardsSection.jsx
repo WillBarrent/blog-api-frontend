5205542f096e6c7a7ba80848eb1e7f6c5a847aab
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-16 16:08:55 +0500

    Fetch posts from database and display it

--- src/Components/BlogCardsSection/BlogCardsSection.jsx
+++ src/Components/BlogCardsSection/BlogCardsSection.jsx
@@ -1,17 +1,30 @@
 import styles from "./BlogCardsSection.module.css";
 import BlogCard from "../BlogCard/BlogCard";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function BlogCardsSection() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+
+    fetch("http://localhost:3000/api/posts", {
+      mode: "cors",
+    }).then(result => result.json()).then((data) => {
+      if (!ignore) {
+        setPosts(data.posts);
+      }
+    });
+
+    return () => {
+      ignore = true;
+    }
+  }, []);
+
   return (
     <div className={styles.blogCards}>
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
+      {posts.map((post) => <BlogCard key={post.id} postId={post.id} title={post.title} createdAt={post.createdAt} />)}
     </div>
   );
 }
