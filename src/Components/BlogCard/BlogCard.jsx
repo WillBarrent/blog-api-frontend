357c0c8ab67de1a0b73cd94da3fbf8725c33d37e
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-16 16:09:39 +0500

    Display all data from database (title, createdAt)

--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -1,17 +1,18 @@
+import { Link } from "react-router-dom";
 import styles from "./BlogCard.module.css";
 import { MessageCircle } from "lucide-react";
 
-function BlogCard() {
+function BlogCard({ postId, createdAt, title }) {
   return (
     <div className={styles.blogCard}>
       <div className={styles.blogCardContent}>
         <div className={styles.blogCardAuthor}>Will Barrent</div>
         <div className={styles.blogCardCreatedAt}>Aug 24, 2025</div>
         <div className={styles.blogCardTitle}>
-        I Tried 20+ Python Courses on Udemy: Here are 5 Courses I recommend
+          <Link to={`/posts/${postId}`} className={styles.blogCardTitleLink}>{title}</Link>
         </div>
         <div className={styles.blogCardComment}>
-          <MessageCircle size={16}/>
+          <MessageCircle size={16} />
           <div className={styles.blogCardCommentPage}>Add Comment</div>
         </div>
       </div>
