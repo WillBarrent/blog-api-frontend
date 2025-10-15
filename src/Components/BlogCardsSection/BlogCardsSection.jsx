a0a6b0547a028e40bb276f3697bab1709029b71d
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-10-15 21:12:35 +0500

    Do not display the post if it is archived

--- src/Components/BlogCardsSection/BlogCardsSection.jsx
+++ src/Components/BlogCardsSection/BlogCardsSection.jsx
@@ -11,20 +11,33 @@ function BlogCardsSection() {
 
     fetch("http://localhost:3000/api/posts", {
       mode: "cors",
-    }).then(result => result.json()).then((data) => {
-      if (!ignore) {
-        setPosts(data.posts);
-      }
-    });
+    })
+      .then((result) => result.json())
+      .then((data) => {
+        if (!ignore) {
+          setPosts(data.posts);
+        }
+      });
 
     return () => {
       ignore = true;
-    }
+    };
   }, []);
 
   return (
     <div className={styles.blogCards}>
-      {posts.map((post) => <BlogCard key={post.id} postId={post.id} title={post.title} createdAt={post.createdAt} />)}
+      {posts.map((post) =>
+        post.published ? (
+          <BlogCard
+            key={post.id}
+            postId={post.id}
+            title={post.title}
+            createdAt={post.createdAt}
+          />
+        ) : (
+          <></>
+        )
+      )}
     </div>
   );
 }
