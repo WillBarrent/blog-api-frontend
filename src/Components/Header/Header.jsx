79b83deea4c5f3ab919dd29673415fdf9f3ad779
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-10-16 23:00:21 +0500

    Dont let unauthorized user create comment

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,32 +8,8 @@ function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
-        <div className={styles.headerContentPrimary}>
-          <h1 className={styles.headerLogo}>
-            <Link className={styles.headerLinkToMainPage} to="/"><div>DEV</div></Link>
-          </h1>
-          <div className={styles.headerSearchBar}>
-            <Search className={styles.headerSearchBarBtn} size={40} />
-            <input
-              type="text"
-              placeholder="Search..."
-              className={styles.headerSearchBarInput}
-            />
-          </div>
-        </div>
-        <div className={styles.headerProfile}>
-          {isUserLoggedIn
-            ?
-            (<div className={styles.headerProfileInfo}>Profile</div>)
-            :
-            (
-              <>
-                <div className={styles.headerLogin}><Link className={styles.headerLoginLink} to="/login">Login</Link></div>
-                <div className={styles.headerSignUp}><Link className={styles.headerSignUpLink} to="/sign-up">Create Account</Link></div>
-              </>
-            )
-          }
-        </div>
+        <div className={styles.headerLogo}>DEV</div>
+        <button className={styles.headerProfile}>Profile</button>
       </div>
     </header>
   );
